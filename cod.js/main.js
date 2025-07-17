9a7527768c1356c443ae80c384f2dcfd4e386733
Lidia Silva da Cruz <20231041110011@ifrn.local>
2025-07-17 17:52:32 -0300

    JS

---
+++ cod.js/main.js
@@ -0,0 +1,9 @@
+const pessoas = [
+    {nome: 'Tiago', idade: 29}
+    {nome: 'Lidia', idade: 18}
+    {nome: 'Will', idade: 17}
+    {nome: 'Acsa', idade: 18}
+    {nome: 'Diego', idade: 29}
+]
+
+const pessoasComNomeGrande = pessoas 
